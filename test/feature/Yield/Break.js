d90dc02bb1dac3a3a0edd18659aa7a16b02f24bb
Nick Winter <livelily@gmail.com>
2014-02-01 10:28:17 -0800

    Added failing test case for #605.

--- test/feature/Yield/Break.js
+++ test/feature/Yield/Break.js
@@ -46,3 +46,52 @@ g = f2();
 assert.deepEqual(g.next(), {value: 3, done: false});
 assert.deepEqual(g.next(), {value: undefined, done: true});
 assert.equal(x, ':abcdh');
+
+
+x = ':';
+
+function* f3() {
+  label1: {
+    x += 'a';
+    while (true) {
+      x += 'b';
+      label2: {
+        x += 'c';
+        yield 5;
+        if (x === ':abc')
+          break label2;
+        x += 'd';
+        while (true) {
+          break label1;
+        }
+        x += 'e';
+      }
+      x += 'f';
+    }
+    x += 'g';
+  }
+  x += 'h';
+}
+
+g = f3();
+assert.deepEqual(g.next(), {value: 5, done: false});
+assert.deepEqual(g.next(), {value: 5, done: false});
+assert.deepEqual(g.next(), {value: undefined, done: true});
+assert.equal(x, ':abccdh');
+
+
+function* f4() {
+  label0: {
+    while(true) {
+      label1: {
+        yield 'something';
+        break label1;
+      }
+    }
+  }
+}
+
+g = f4();
+assert.deepEqual(g.next(), {value: 'something', done: false});
+assert.deepEqual(g.next(), {value: 'something', done: false});
+assert.deepEqual(g.next(), {value: 'something', done: false});
